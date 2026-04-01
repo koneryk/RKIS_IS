8076e5bdbf62201eef60cd8bfef6c2a130cba8f8
unknown <@st305-13.ad.rb.asu.ru>
2026-04-01 08:57:48 +0700

    validation process

--- IS/client/dev-dist/sw.js
+++ IS/client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6fc00345'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.c0phjquggek"
+    "revision": "0.6sm1ed7iej"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- IS/client/src/views/ClientsADD.vue
+++ IS/client/src/views/ClientsADD.vue
@@ -2,15 +2,17 @@
     <div>
         <div>
             <p>Введите наимекнование</p>
-            <input type="text" name="NAME" id="NAME_cl">
+            <input type="text" name="NAME" id="NAME_cl" v-model="names">
             <p>Введите ИНН</p>
-            <input type="number" name="INN" id="INN_cl">
+            <input type="number" name="INN" id="INN_cl" v-model="inns" >
             <p>Введите ОГРН</p>
-            <input type="text" name="OGRN" id="OGRN_cl">
+            <input type="text" name="OGRN" id="OGRN_cl" v-model="ogrns">
             <p>Введите Адрес</p>
-            <input type="text" name="ADRESS" id="ADRESS_cl">
+            <input type="text" name="ADRESS" id="ADRESS_cl" v-model="adresss">
             <p>Введите Телефон</p>
-            <input type="number" name="TELEPHONE" id="TELEPHONE_cl">
+            <input type="number" name="TELEPHONE" id="TELEPHONE_cl" v-model="phones">
+            <p>Введите email</p>
+            <input type="number" name="EMAIL" id="EMAIL_cl" v-model="emails">
 
             <button @click="addclients">Добавить клиента</button>
         </div>
@@ -24,15 +26,42 @@ import api from '../api';
 const route = useRoute();
 const router = useRouter();
 
-const clients = ref([]);
-const products = ref([]);
+
+//^[\w\s\d\-А-яЁё]+$
+const names = ref('')
+
+
+//^(\d{10}|\d{12})$
+const inns = ref()
+
+//^(\d{13}|\d{15})$
+const ogrns = ref()
+
+//^[а-яА-ЯёЁa-zA-Z0-9\s\.,\-\/№]+$
+const adresss = ref()
+
+//^(\+7|8)?\s?\(?\d{3}\)?[\s-]?\d{3}[\s-]?\d{2}[\s-]?\d{2}$
+const phones = ref()
+
+///^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+const emails = ref()
+
+
+
+
 const loading = ref(true);
 const error = ref('');
 const submitting = ref(false);
 const name_cl = ref('')
 
+// const { name, inn, ogrn, address, phone, email } = req.body;
+async function addclients(){
+    //Валидация наименования
+    let regi = '^[\w\s\d\-А-яЁё]+$'
+    let res = names.value.match(regi)
 
 
+}
 
 
 /*
